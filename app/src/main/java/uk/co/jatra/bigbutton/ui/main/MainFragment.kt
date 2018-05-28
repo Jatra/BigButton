a45712c97e4dde2e8c365b65d78a06d5e708ca61
Tim Russell <tim@jatra.co.uk>
2018-05-28 14:07:58 +0100

    Radio buttons

--- app/src/main/java/uk/co/jatra/bigbutton/ui/main/MainFragment.kt
+++ app/src/main/java/uk/co/jatra/bigbutton/ui/main/MainFragment.kt
@@ -10,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import uk.co.jatra.bigbutton.R
 
@@ -28,16 +29,27 @@ class MainFragment : Fragment() {
         val view = inflater.inflate(R.layout.main_fragment, container, false)
         val button = view.findViewById<Button>(R.id.button)
 
-        val firstLine = "One Way."
-        val length1 = firstLine.length
-        val secondLine = "Pick up now, drop off flexibly."
-        val length2 = secondLine.length
+        val firstLine = "Big."
+        val secondLine = "little stuff"
         var text = SpannableString(firstLine + "\n" + secondLine)
 
         text.setSpan(AbsoluteSizeSpan(16, true), 0, firstLine.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         text.setSpan(AbsoluteSizeSpan(12, true), firstLine.length+1, firstLine.length+1+secondLine.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
         button.setText(text, TextView.BufferType.SPANNABLE)
+
+        val psuedoButton = view.findViewById<LinearLayout>(R.id.pseudoButton)
+        val psuedoButton2 = view.findViewById<LinearLayout>(R.id.pseudoButton2)
+        psuedoButton.isSelected = true
+        psuedoButton.setOnClickListener {
+            psuedoButton.isSelected = true
+            psuedoButton2.isSelected = false
+        }
+        psuedoButton2.isSelected = false
+        psuedoButton2.setOnClickListener {
+            psuedoButton2.isSelected = true
+            psuedoButton.isSelected = false
+        }
         return view
     }
 
